0c6081878186fa92e66a337831dc44ee20de69e2
Robert Östberg <robbanost@gmail.com>
2022-11-01 02:42:35 +0100

    Skapat en mer riktad approach för att maximera applikationerna

---
+++ test.ps1
@@ -0,0 +1,18 @@
+
+$wshell = New-Object -ComObject wscript.shell; # skapar ett ComObject som kan skicka tangent-tryck
+Start-Sleep 120 # sekunder innan resterande script 
+$FindProcess = Get-Process | Where-Object {$_.MainWindowTitle -ne ""} | Select-Object MainWindowTitle #Get-Process | Where-Object {$_.MainWindowTitle -like "*$processterm*"}
+$tvs = @()
+# Write-Output $FindProcess
+foreach($window in $FindProcess){
+    if( $window.MainWindowTitle.Contains('TV')){
+        $tvs += $window
+    }
+}
+# Write-Output $tvs.Count
+foreach($window in $tvs){
+    $wshell.AppActivate($window.MainWindowTitle.ToString())
+    $wshell.SendKeys('{F11}')
+    Start-Sleep 2
+    $wshell.SendKeys('{F11}')  
+}
