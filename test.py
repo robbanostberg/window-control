9d49a2c02c8030e70b17e19485fef58e68f75c62
Robert Östberg <robbanost@gmail.com>
2022-11-04 16:18:24 +0100

    Ändrat några värden för att testa på riktigt

--- test.py
+++ test.py
@@ -5,15 +5,17 @@ from time import sleep as wait
 
 mouse = mouseCon()
 kb = keyCon()
-marginal = 0.3
-startup = 10
+marginal = 0.2
+startup = 90
 delay = 0.5
 
+print("Waiting for " + startup + " seconds")
 wait(startup)
 
 for m in get_monitors():
     mouse.position = (m.x + int(m.width * marginal), m.y + int(m.height * marginal))
     mouse.click(Button.left)
+    wait(delay)
     kb.tap(Key.f11)
     wait(delay)
     kb.tap(Key.f11)
