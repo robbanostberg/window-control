0deecc04265cb7d3e40c89628bbd70ad87b04649
Robert Östberg <robbanost@gmail.com>
2023-01-15 00:41:58 +0100

    Lagt till kommentarer samt ökat startfördröjning

--- Fullscreen.py
+++ Fullscreen.py
@@ -1,52 +1,28 @@
-import time
-from pynput.keyboard import Key, Controller
-from pywinauto import Desktop
+from screeninfo import get_monitors
+from pynput.mouse import Controller as mouseCon, Button
+from pynput.keyboard import Controller as keyCon, Key
+from time import sleep as wait
+
+mouse = mouseCon()  # Skapar musobjekt
+kb = keyCon()  # Skapar tangentbordsobjekt
+marginal = 0.2  # Avstånd från kanten av skärmen
+startup = 120  # Väntetid från start till exekvering
+delay = 0.5  # Paus mellan tryck
+
+print("Waiting for " + str(startup) + " seconds before executing F11 presses to send FAST TV windows into fullscreen mode")  # Berättar vad som pågår för de som ser skärmen
+wait(startup)  # Väntar så de andra programmen hinner starta
+
+for m in get_monitors():  # Tar reda på antalet skärmar och gör följande för varje skärm:
+    mouse.position = (m.x + int(m.width * marginal), m.y + int(m.height * marginal))  # Placerar musen 20% in uppifrån och vänsterifrån på skärmen
+    mouse.click(Button.left)  # Klickar en gång med musen för att aktivera programfönstret
+    wait(delay)
+    kb.tap(Key.f11)  # Trycker F11 två gånger
+    wait(delay)
+    kb.tap(Key.f11)
+
+   
 
 
-"""-------- static variables --------"""
-debug = False  # set to True to get printouts and deactivate the F11 presses
-wait = 0.5  # seconds
-startup = 120  # seconds
-interval = 10  # seconds
-windows2skip = 3  # amount of detected windows that aren't actual windows
 
 
-"""-------- helper functions --------"""
-def pressKey(controller, key):
-    controller.press(key)
-    time.sleep(wait)
 
-def tapKey(controller, key):
-    controller.tap(key)
-    time.sleep(wait)
-
-def releaseKey(controller, key):
-    controller.release(key)
-    time.sleep(wait)
-
-def startDelay():
-    for i in range(0, startup, interval):
-        print(startup - i)
-        time.sleep(interval)
-
-
-"""-------- to be executed --------"""
-if not debug:
-    startDelay()  # delayes execution of script for 3 minutes (to be evaluated)
-
-windows = Desktop(backend="uia").windows()  # creates a list of windows (and 4 other things)
-if debug:
-    for w in windows:
-        print(w.window_text())  # prints name of every window if debug is True
-    print(len(windows),'\n')  # prints the size of the list of windows if debug is True
-
-k = Controller()  # creates a controller instance to send key strokes
-
-for e in range(len(windows)-windows2skip):  # does *see below* for size of windows list - 4 
-    if not debug:
-        tapKey(k, Key.f11)  # taps the F11 key twice with 0.5 s between, if debug is False
-        tapKey(k, Key.f11)
-    pressKey(k, Key.alt)  # holds the ALT key down
-    for i in range(e):  # presses the TAB key an increasing amount of times to iterate through each window
-        tapKey(k, Key.tab)
-    releaseKey(k, Key.alt)  # releases the ALT key
